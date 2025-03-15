6fd245039525463fa6cfbd628fed0eb4864a7c01
jerome provensal <552382+jeromegit@users.noreply.github.com>
2025-03-14 22:10:12 -0700

    Add details on how to get the headers for the session

--- rei_purchase_history_using_cookies.py
+++ rei_purchase_history_using_cookies.py
@@ -17,7 +17,9 @@ def get_rei_session():
     session = requests.Session()
     session.cookies.update(cookies)
 
-    # these should be extracted using the DevTools
+    # these should be extracted using the DevTools > Network
+    # then use "Copy as fetch" for most
+    # "Copy as cURL" for the user-agent value
     headers = {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "accept-language": "en-US,en;q=0.9,fr-FR;q=0.8,fr;q=0.7,es;q=0.6",
