449949f06c17196ea711f9a7a326011f8a7e459e
jerome provensal <552382+jeromegit@users.noreply.github.com>
2025-03-15 19:04:38 -0700

    before trying a better approach of extracting/parsing instore purchase item(s)

--- rei_purchase_history_using_cookies.py
+++ rei_purchase_history_using_cookies.py
@@ -1,4 +1,8 @@
 import json
+from dataclasses import dataclass
+from datetime import date, datetime
+from decimal import Decimal
+from typing import Union, Optional
 
 import browser_cookie3
 import requests
@@ -8,6 +12,20 @@ from bs4 import BeautifulSoup
 
 # IMPORTANT FIRST STEP BEFORE RUNNING ANY OF THIS...
 # Manually login to rei.com using Chrome
+
+def convert_date_format(date_string):
+    try:
+        # Parse the date string
+        date_obj = datetime.strptime(date_string, "%A, %b %d, %Y")
+
+        # Format it to YYYY-MM-DD
+        formatted_date = date_obj.strftime("%Y-%m-%d")
+
+        return formatted_date
+
+    except ValueError as e:
+        return f"Error parsing date: {e}"
+
 
 def get_rei_session():
     site = 'rei.com'
@@ -49,7 +67,7 @@ def get_json_data_for_url(session, url):
     return json_data
 
 
-def get_purchase_items_in_order(session, purchase_order_id):
+def get_online_purchase_items_in_order(session, purchase_order_id):
     # https://www.rei.com/order-details/rs/purchase-details/A319493263
     purchase_items_url = f'https://www.rei.com/order-details/rs/purchase-details/{purchase_order_id}'
     json_data = get_json_data_for_url(session, purchase_items_url)
@@ -58,7 +76,7 @@ def get_purchase_items_in_order(session, purchase_order_id):
     purchase_items = []
     for items in fulfillment_items:
         for item in items:
-            purchase_item = {k: item[k] for k in {'sku', 'name', 'brand', 'totalPrice'}}
+            purchase_item = {k: item[k] for k in {'sku', 'name', 'brand', 'price'}}
             purchase_item['order_date'] = order_date
             purchase_items.append(purchase_item)
 
@@ -66,7 +84,7 @@ def get_purchase_items_in_order(session, purchase_order_id):
 
 
 def print_purchase_items(purchase_items):
-    headers = ["order_date", "brand", "name", "sku", "totalPrice"]
+    headers = ["order_date", "brand", "name", "sku", "price"]
     table_data = [[item.get(key, "") for key in headers] for item in purchase_items]
     print(tabulate(table_data, headers=headers, tablefmt="psql"))
 
@@ -78,28 +96,59 @@ def get_online_purchases_for_year(session, year):
         purchase_order_ids = [order["orderId"] for order in json_data["history"]]
 
         for purchase_order_id in purchase_order_ids:
-            purchase_items = get_purchase_items_in_order(session, purchase_order_id)
+            purchase_items = get_online_purchase_items_in_order(session, purchase_order_id)
             print_purchase_items(purchase_items)
 
 
+def get_single_purchase_item(soup, html):
+    sku = html.find('li', {'data-ui': 'product-sku'})
+    if sku:
+        sku_text = sku.text.strip()
+        sku = sku_text.replace('Item #', '').strip()
+        name = soup.select_one('[data-ui="product-title"] a').text.strip()
+        price = soup.select_one('[data-ui="product-total-price"]').text.strip()
+        order_date = soup.select_one('[data-ui="order-date"]').text.strip()
+    else:
+        sku = name = 'n/a'
+        price = soup.select_one('[data-ui="total-value"]').text
+        order_date = html.find('li', {'data-ui': 'order-date'})
+
+    price = price.replace('$', '')
+    brand = 'n/a'
+    order_date = convert_date_format(order_date)
+    purchase_item = {'sku': sku, 'name': name, 'brand': brand, 'price': price, 'order-date': order_date}
+
+    return purchase_item
+
+
+def get_instore_purchase_items_in_order(session, purchase_order_id):
+    purchase_item_url = f"https://www.rei.com/retail-purchase-details?from_url=PurchaseHistoryView&fromSubmit=true&orderId={purchase_order_id}&retailPurchase=true"
+    response = session.get(purchase_item_url)
+    html = response.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+    purchase_items = []
+    product_sections = soup.find_all('div', class_='product-section')
+    if product_sections:
+        for section in product_sections:
+            purchase_item = get_single_purchase_item(soup, section)
+            purchase_items.append(purchase_item)
+    else:
+        purchase_item = get_single_purchase_item(soup, html)
+        purchase_items.append(purchase_item)
+
+    return purchase_items
+
 
 def get_instore_purchases_for_year(session, year):
-    # purchase_historty_2024_url ='https://www.rei.com/retail-purchase-details?from_url=PurchaseHistoryView&fromSubmit=true&orderId=614011716&retailPurchase=true'
+    purchase_items_url = f"https://www.rei.com/rest/user/orders?year={year}&includeSkus=true&retailOnly=true"
+    json_data = get_json_data_for_url(session, purchase_items_url)
+    purchase_order_ids = [order["csaOrderId"] for order in json_data]
+    print(f"{year} {purchase_order_ids}")
 
-    # soup = BeautifulSoup(html, 'html.parser')
-    #
-    # product_name = soup.select_one('[data-ui="product-title"] a').text.strip()
-    #
-    # # Extract the price (text inside the <li> tag with "data-ui=product-total-price")
-    # product_price = soup.select_one('[data-ui="product-total-price"]').text.strip()
-    #
-    # # Clean the price (remove the "$" sign if needed)
-    # product_price = product_price.replace('$', '')
-    #
-    # # Print extracted data
-    # print(f"Product Name: {product_name}")
-    # print(f"Product Price: {product_price}")
-    pass
+    for purchase_order_id in purchase_order_ids:
+        purchase_items = get_instore_purchase_items_in_order(session, purchase_order_id)
+    print_purchase_items(purchase_items)
 
 
 def get_all_online_purchases(session, years):
@@ -116,6 +165,6 @@ if __name__ == "__main__":
     session = get_rei_session()
 
     years = list(range(2016, 2025))
-    #years = list(range(2024, 2025))
-    get_all_online_purchases(session, years)
+    # years = list(range(2024, 2025))
+    # get_all_online_purchases(session, years)
     get_all_instore_purchases(session, years)
